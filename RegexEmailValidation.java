6a1628c0e25a930feddd80309b559bd0d67b6eb4
Ashwini Khonde <ashwinikhonde98@gmail.com>
2021-01-25 23:01:14 +0530

    ADDED: Message

---
+++ RegexEmailValidation.java
@@ -0,0 +1,12 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexEmailValidation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Email Validation Using Regex");//display the result
+	}
+
+}
