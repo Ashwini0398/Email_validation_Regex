7580856f92ac197c5767dd140d3f1e4cf2384ffe
Ashwini Khonde <ashwinikhonde98@gmail.com>
2021-01-25 23:07:12 +0530

    ADDED: Email validatation for first two mandatory fields

--- RegexEmailValidation.java
+++ RegexEmailValidation.java
@@ -1,3 +1,5 @@
+package practice;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -6,7 +8,10 @@ public class RegexEmailValidation {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Email Validation Using Regex");//display the result
+		Pattern pat =Pattern.compile("^[a-z]+$");//compile the pattern 
+		Matcher m = pat.matcher("abc@bridzelabz.com");//enter the pattern to match
+		boolean result=m.matches();//matches whether it is true or false
+		System.out.println(result);//display the result
 	}
 
 }
