0d8887c02c4c41586b6884eead2e6823995b79f9
Ashwini Khonde <ashwinikhonde98@gmail.com>
2021-01-26 07:16:29 +0530

    ADDED: Email Validation of special character

--- RegexEmailValidation.java
+++ RegexEmailValidation.java
@@ -3,15 +3,18 @@ package practice;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class RegexEmailValidation {
+public class RegexEmailValidation 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
 		
-		
-		Pattern pat =Pattern.compile("^[a-zA-Z0-9]*[+._-]*[a-zA-Z0-9]+@[a-zA-Z]+.{3}[a-zA-z.]+$");//compile the pattern
-                Matcher m = pat.matcher("abc@bridzelabz.com");//enter the pattern to match
-                boolean result=m.matches();//matches whether it is true or false
-                System.out.println(result);//display the result
+		Pattern pat =Pattern.compile("^([a-zA-Z0-9]*[+._-]*[a-zA-Z0-9]+@[a-zA-Z]+.{3}[a-zA-z.]*[a-zA-z]{2})+$");//compile the pattern 
+		Matcher m = pat.matcher("abc.xyz@birdgelabz.co.in");//enter the pattern to match
+		boolean result=m.matches();//matches whether it is true or false
+		System.out.println(result);//display the result		
+
 	}
 
 }
