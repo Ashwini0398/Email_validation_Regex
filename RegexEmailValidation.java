f13f2231e8cb95550794e3e93824c8a3272b184b
Ashwini Khonde <ashwinikhonde98@gmail.com>
2021-01-26 07:11:42 +0530

    ADDED: email validation for 3rd part

--- RegexEmailValidation.java
+++ RegexEmailValidation.java
@@ -8,8 +8,8 @@ public class RegexEmailValidation {
 	public static void main(String[] args) {
 		
 		
-		Pattern pat =Pattern.compile("^[a-zA-z]*@[a-zA-Z]+$");//compile the pattern
-                Matcher m = pat.matcher("abc@bridzelabz");//enter the pattern to match
+		Pattern pat =Pattern.compile("^[a-zA-Z0-9]*[+._-]*[a-zA-Z0-9]+@[a-zA-Z]+.{3}[a-zA-z.]+$");//compile the pattern
+                Matcher m = pat.matcher("abc@bridzelabz.com");//enter the pattern to match
                 boolean result=m.matches();//matches whether it is true or false
                 System.out.println(result);//display the result
 	}
